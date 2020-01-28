5a8b10c381dbe6619b1ce16731bc698c06122103
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-01-28 09:50:53 +0530

    Changed Variable Name

--- hand_cricket.py
+++ hand_cricket.py
@@ -1,7 +1,7 @@
 import random as random
 score = []
-overs = int(input("enter no. of overs"))
-for i in range(overs*6):
+overss = int(input("enter no. of overs"))
+for i in range(overss*6):
     print("DElivery %d:", (i+1))
     player = int(input("enter score on the ball played"))
     comp = random.randrange(0, 6, 1)
